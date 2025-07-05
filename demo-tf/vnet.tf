46758c13eee5904688f1ef8f8a83a7234091341c
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-05 18:01:15 +0530

    error modified creation of subnets

--- demo-tf/vnet.tf
+++ demo-tf/vnet.tf
@@ -17,20 +17,20 @@ resource "azurerm_subnet" "subnet-app"{
     name = "Myappsubnet-tf"
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = ["10.0.1.0/24"]
+    address_prefixes = ["10.0.0.0/28"]
 }
 
 resource "azurerm_subnet" "subnet-web"{
     name = "Mywebsubnet-tf"
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = ["10.0.2.0/24"]
+    address_prefixes = ["10.0.0.1/28"]
 }
 
 resource "azurerm_subnet" "subnet-db"{
     name = "Mydbsubnet-tf"
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
-    address_prefixes = ["10.0.3.0/24"]
+    address_prefixes = ["10.0.0.2/28"]
 }
 
