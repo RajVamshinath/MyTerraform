fcff41563c6b5be0421e6ddf5915bfb9ba8b7528
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-05 18:35:52 +0530

    creation of nsgs

--- demo-tf/vnet.tf
+++ demo-tf/vnet.tf
@@ -34,3 +34,58 @@ resource "azurerm_subnet" "subnet-db"{
     address_prefixes = ["10.0.0.32/28"]
 }
 
+# creation of network security groups
+resource "azurerm_network_security_group" "nsg-app" {
+    name = "Myappnsg-tf"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule{
+        name = "AllowAppTraffic"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "8080"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_network_security_group" "nsg-web" {
+    name = "Mywebnsg-tf"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule{
+        name = "AllowWebTraffic"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "80"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
+resource "azurerm_network_security_group" "nsg-db" {
+    name = "Mydbnsg-tf"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    security_rule{
+        name = "AllowDbTraffic"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "5432"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+}
+
