f8c794afd6320b7103e9e59fbfe87a7550d4cd7a
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-05 17:40:16 +0530

    Vnet created

--- demo-tf/vnet.tf
+++ demo-tf/vnet.tf
@@ -1,4 +1,15 @@
+# create a resource group
 resource "azurerm_resource_group" "rg" {
     name = "Myrg-tf"
     location = "East US"
 }
+
+# create a virtual network
+resource "azurerm_virtual_network" "vnet"{
+    name = "Myvnet-tf"
+    location = "azurerm_resource_group.rg.location"
+    resource_group_name = "azurerm_resource_group.rg.name"
+    address_space = ["10.0.0.0/24"]
+}
+
+
