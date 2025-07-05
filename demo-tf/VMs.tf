24846a6e31eb4418f12bfe75725e75fd1372c3bc
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-05 20:46:11 +0530

    error modified creation of NIC and App-vm

--- demo-tf/VMs.tf
+++ demo-tf/VMs.tf
@@ -16,17 +16,17 @@ resource "azurerm_linux_virtual_machine" "App-vm" {
     resource_group_name = azurerm_resource_group.rg.name
     location = azurerm_resource_group.rg.location
     size = "Standard_B1s"
+    network_interface_ids = [azurerm_network_interface.App-nic.id]
     admin_username = "azureuser"
     admin_password = "Azureuser@1234"
     disable_password_authentication = "false"
-    network_interface_ids = [azurerm_network_interface.App-nic.id]
     os_disk {
         caching = "ReadWrite"
         storage_account_type = "Standard_LRS"
     }
-    source_image_reference{
+    source_image_reference {
         publisher = "Canonical"
-        offer = "UbuntuServer"
+        offer = "0001-com-ubuntu-server-jammy"
         sku = "22.04-LTS"
         version = "latest"
     }
