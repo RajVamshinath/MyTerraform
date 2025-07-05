bae052991c43783e51ccc9b31af4297d1835619c
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-05 17:44:04 +0530

    error modified vnet created

--- demo-tf/vnet.tf
+++ demo-tf/vnet.tf
@@ -7,8 +7,8 @@ resource "azurerm_resource_group" "rg" {
 # create a virtual network
 resource "azurerm_virtual_network" "vnet"{
     name = "Myvnet-tf"
-    location = "azurerm_resource_group.rg.location"
-    resource_group_name = "azurerm_resource_group.rg.name"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
     address_space = ["10.0.0.0/24"]
 }
 
