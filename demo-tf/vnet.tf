82a607968756bb5978e500f275583776af8c4ffc
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-09 15:37:13 +0530

    Created vars.tf and modified vnet.tf

--- demo-tf/vnet.tf
+++ demo-tf/vnet.tf
@@ -17,7 +17,7 @@ resource "azurerm_virtual_network" "vnet"{
 resource "azurerm_subnet" "subnet-app"{
     name = "Myappsubnet-tf"
     resource_group_name = azurerm_resource_group.rg.name
-    virtual_network_name = azurerm_virtual_network.vnet[each.key]
+    virtual_network_name = azurerm_virtual_network.vnet[each.key].name
     address_prefixes = ["10.0.0.0/28"]
 }
 
