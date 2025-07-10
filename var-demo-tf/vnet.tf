3f9875fe867767c3c1685facbd9aa19a7de7559f
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-10 22:03:15 +0530

    error modified in vnet.tf

--- var-demo-tf/vnet.tf
+++ var-demo-tf/vnet.tf
@@ -16,7 +16,7 @@ resource "azurerm_virtual_network" "vnet"{
 resource "azurerm_subnet" "subnet_public"{
     for_each = var.subnet_public_CIDR
     name = each.key
-    address_prefixes = each.value
+    address_prefixes = [each.value]
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
 }
