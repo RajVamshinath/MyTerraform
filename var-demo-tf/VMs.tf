53305e0554ca56a4d0fa601abcd8a723cdcf1ff9
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-11 19:39:10 +0530

    created VM

--- var-demo-tf/VMs.tf
+++ var-demo-tf/VMs.tf
@@ -23,3 +23,25 @@ resource "azurerm_network_interface" "private_NIC"{
         private_ip_address_allocation = var.private_nic[each.key].private_ip_address_allocation
     }
 }
+
+# create Virtual machine in public subnet for App
+resource "azurerm_linux_virtual_machine" "App-vm" {
+    name = "App-vm-tf"
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
+    size = "Standard_B1s"
+    network_interface_ids = [azurerm_network_interface.public_NIC.id]
+    admin_username = "azureuser"
+    admin_password = "Azureuser@1234"
+    disable_password_authentication = "false"
+    os_disk {
+        caching = "ReadWrite"
+        storage_account_type = "Standard_LRS"
+    }
+    source_image_reference {
+        publisher = "Canonical"
+        offer = "0001-com-ubuntu-server-jammy"
+        sku = "22_04-lts"
+        version = "latest"
+    }
+}
