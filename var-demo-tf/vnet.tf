338465edaa103e8a6387bd5d6857819fe517f422
RajVamshinath <rajvamshinath16@gmail.com>
2025-07-11 15:02:50 +0530

    error fixed

--- var-demo-tf/vnet.tf
+++ var-demo-tf/vnet.tf
@@ -80,7 +80,7 @@ resource "azurerm_subnet_network_security_group_association" "public_subnet_NSG_
 # associate private subnets with their respective NSGs
 resource "azurerm_subnet_network_security_group_association" "private_subnet_NSG_association"{
     for_each =  var.subnet_private_CIDR
-    subnet_id = azurerm_subnet.subnet_private_id[each.key].id
+    subnet_id = azurerm_subnet.subnet_private_db[each.key].id
     network_security_group_id = azurerm_network_security_group.nsg_private[each.key].id
 }
 
@@ -94,6 +94,6 @@ resource "azurerm_nat_gateway" "nat-private_subnet"{
 # associate NAT with private subnets
 resource "azurerm_subnet_nat_gateway_association" "nat_private_subnet_association"{
     for_each = var.subnet_private_CIDR
-    subnet_id = azurerm_subnet.subnet_private_id[each.key].id
+    subnet_id = azurerm_subnet.subnet_private_db[each.key].id
     nat_gateway_id = azurerm_nat_gateway.nat-private_subnet.id
 }
